bea0fb6c2f7b1fb1cc92220aff46db0a5b4145a6
FujiHaruka <fujiharuka@realglobe.jp>
2016-11-21 00:04:40 +0900

    The epistle of barnabas

---
+++ src/index.js
@@ -0,0 +1,19 @@
+module.exports = [
+  {
+    id: 1,
+    title: {
+      en: 'The Epistle of Barnabas',
+      ja: 'バルナバの手紙'
+    },
+    directory: 'the_epistle_of_barnabas',
+    author: {
+      en: 'Barnabas',
+      ja: 'バルナバ'
+    },
+    authorInfo: '',
+    writtenYear: '80-190',
+    description: '使徒教父文書',
+    comment: '',
+    originalUrl: 'http://www.newadvent.org/fathers/0124.htm'
+  }
+]
---
+++ src/the_epistle_of_barnabas/chapter_19/index.js
@@ -0,0 +1,6 @@
+const chapterSrc = require('../index.js')
+const article = require('./index.json')
+
+module.exports = Object.assign({
+  article
+}, chapterSrc)
